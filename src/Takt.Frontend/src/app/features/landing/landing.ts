182139377fd7095ab2588bf8dad7852dbe155691
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-09-04 00:55:20 +0300

    feat: link to the workspace from the landing page when signed in

--- src/Takt.Frontend/src/app/features/landing/landing.ts
+++ src/Takt.Frontend/src/app/features/landing/landing.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { APP_ROUTES } from '../../constants/routes.constants';
+import { AuthService } from '../../core/auth/auth.service';
 import { Header } from '../../shared/components/header/header';
 
 @Component({
@@ -10,4 +11,5 @@ import { Header } from '../../shared/components/header/header';
 })
 export class Landing {
   protected readonly routes = APP_ROUTES;
+  protected readonly isAuthenticated = inject(AuthService).isAuthenticated;
 }
